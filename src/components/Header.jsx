0c1461706dab90f6a3db469723afec284c5cde95
gaurav060404 <gs762282@gmail.com>
2026-01-03 16:16:52 +0530

    add Header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+      <div className='flex justify-center items-center h-1/12 w-full border-2'>
+        Header  
+    </div>
+  )
+}
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Header from './Header.jsx'
 
 export default function MainSection() {
   return (
-    <div className='flex justify-center items-center w-full border-2'>MainSection</div>
+    <div className='flex flex-col w-full'>
+      <Header />
+      <div className='flex justify-center items-center h-full w-full border-2'>
+        Main Section
+      </div>
+    </div>
   )
 }
